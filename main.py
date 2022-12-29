cc94ead71cafefaa426b6c1ebd3584d6f4473cae
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-12-29 23:11:40 +0500

    deposite fun is complete

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 def deposite():
     while True:
-      amout = input("What Would You Like To Deposite? $")
-      if amount.isdigit():
+        amount = input("What Would You Like To Deposite? $")
+        if amount.isdigit():
+            amount = int(amount)
+
+            if amount > 0:
+                break
+            else:
+                print("Amount Must be Greater Then Zero")
+        else:
+            print("Enter a Number.")
+
+    return amount
