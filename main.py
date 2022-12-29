1b4ba1571ca40a285e5ab48864cc209c2073296d
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-12-29 23:41:50 +0500

    input-funcs-complete

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
 
-
+#! Get the Deposite From User...
 def deposite():
     while True:
         amount = input("What Would You Like To Deposite? $ ")
@@ -17,6 +19,7 @@ def deposite():
     return amount
 
 
+#! Get the number of Lines...
 def get_number_of_lines():
     while True:
         lines = input("Enter the Number of Lines to Bet on(1-" + str(MAX_LINES) + ")? ")
@@ -33,11 +36,43 @@ def get_number_of_lines():
     return lines
 
 
+#! Get the Bet
+
+
+def get_bet():
+    while True:
+        amount = input("What Would You Like To Bet on each Line? $ ")
+        if amount.isdigit():
+            amount = int(amount)
+
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount Must be Between {MIN_BET} - {MAX_BET}")
+        else:
+            print("Enter a Number.")
+
+    return amount
+
+
+#! Main Funcation
 def main():
     balance = deposite()
     lines = get_number_of_lines()
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
 
-    print(balance, lines)
+        if total_bet > total_bet:
+            print(
+                f"You don't have enough to bet on that amount, Your Current Balance is: ${balance}"
+            )
+        else:
+            break
+
+    print(
+        f"You are Betting ${bet} on {lines} lines. Total bet is equal to ${total_bet}"
+    )
 
 
 main()
